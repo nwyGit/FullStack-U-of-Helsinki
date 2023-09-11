ac90663f1d133fb325256c78f7189d452078748c
Wai Yan Ng <nwyraymond@gmail.com>
2023-09-11 11:01:17 -0400

    Complete 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { voteIncrement } from '../reducers/anecdoteReducer';
+import Filter from './Filter';
 
 const AnecdoteList = () => {
-	const anecdotes = useSelector((state) => state);
+	const anecdotes = useSelector((state) =>
+		state.anecdotes.filter((a) => a.content.includes(state.filter))
+	);
 	const dispatch = useDispatch();
 
 	const vote = (id) => {
@@ -12,6 +15,7 @@ const AnecdoteList = () => {
 	};
 	return (
 		<>
+			<Filter />
 			{anecdotes.map((anecdote) => (
 				<div key={anecdote.id}>
 					<div>{anecdote.content}</div>
